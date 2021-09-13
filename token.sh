d76fb883aa74ac72a6d1fef1aee514c0f57b0c09
Josef Johansson <josef@oderland.se>
2021-09-13 11:52:24 +0200

    token.sh: Add jws inspector
    Signed-off-by: Josef Johansson <josef@oderland.se>

--- token.sh
+++ token.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+token="$(echo "$1" | jq -c)"
 
+echo "$token" | docker run --rm -v $PWD/token:/token -i --entrypoint step smallstep/step-ca crypto jws inspect --insecure
 docker-compose exec step-ca step ca certificate \
 	--root /etc/ssl/certs/ca-certificates.crt \
-	--token "$1" \
+	--token "$token" \
 	--ca-url https://step-ca \
 	--provisioner hydra  \
 	foo@bar.com /output/p.crt /output/p.key
