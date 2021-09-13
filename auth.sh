466ac474826f18ad5c9ae3f887430584530d5c42
Josef Johansson <josef@oderland.se>
2021-09-13 11:51:20 +0200

    auth.sh: Change to use functions instead, also fixed the problem!
    Turns out I adden openid=on instead of grant_scope=openid in post to consent
    Now I get a id_token.
    
    Signed-off-by: Josef Johansson <josef@oderland.se>

--- auth.sh
+++ auth.sh
@@ -2,192 +2,172 @@
 
 set -euo pipefail
 
-data="$(docker-compose run --rm curl -i --cacert /etc/ssl/certs/ca-certificates.crt "$1")"
-
-oauth2_authentication_csrf="$(echo -e "$data" | grep -oP '(?<=: oauth2_authentication_csrf=)[^;]+' | tr -d '\r')"
-location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
-challenge="$(echo $location | cut -d '=' -f 2)"
-code="$(echo -e "$data" | grep -oP '(?<=HTTP/2 )[0-9]+' | tr -d '\r')"
-
-if [ "$code" == "302" ]; then
-	printf '[1] %s\n' \
-		"Asked Hydra for a Oauth2 Authentication CSRF Cookie" \
-	        "Was redirected to consent app" \
-		""
-else
-	printf '[1] %s\n' \
-		"Asked Hydra for a Oauth2 Authentication CSRF Cookie" \
-		"Got $code instead of 302" \
-		""
-	exit 1
-fi
-
-
-data="$(docker-compose run --rm curl -i "$location")"
-
-code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
-h_csrf="$(echo -e "$data" | grep -oP '(?<=: _csrf=).+$' | tr -d '\r')"
-f_csrf="$(echo -e "$data" | grep -oP '(?<=_csrf" value=")[^"]+')"
-
+declare -a cookies
+cookies=()
+old_location=
+default=(-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' '--cacert' '/etc/ssl/certs/ca-certificates.crt' '-i' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: cross-site' -H 'Sec-Fetch-User: ?1' -H 'TE: trailers' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.5' -H 'Connection: Keep-alive' --compressed)
 user='foo%40bar.com'
 pass='foobar'
-raw="_csrf=$f_csrf&challenge=$challenge&email=$user&password=$pass&submit=Log+in"
+data=
 
-if [ "$code" == "200" ]; then
-	printf '[2] %s\n' \
-		"Getting the login form at the consent app" \
-		"It worked, logging in" \
-		""
-else
-	printf '[2] %s\n' \
-		"Getting the login form at the consent app" \
-		"he consent app" \
-		"Got $code instead of 200" \
-		""
-	exit 1
-fi
+function log()
+{
+    printf '%s\n' "$@"
+}
 
-data="$(docker-compose run --rm curl \
-    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    -H 'Origin: http://consent:3000' \
-    -H "Referer: http://consent:3000/login?login_challenge=$challenge" \
-    -H "Cookie: _csrf=$h_csrf" \
-    --data-raw "$raw" -i \
-    "$location")"
+function iinfo()
+{
+    i="$1"; shift
+        info "info: [$i] $@"
+}
 
-code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
-location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
+function idie()
+{
+    i="$1"; shift
+        die "$i" "info: [$i] $@"
+}
 
-if [ "$code" == "302" ]; then
-	printf '[3] %s\n' \
-		"Logging in at the consent app" \
-		"It worked, redirected towards Hydra" \
-		""
-else
-	printf '[3] %s\n' \
-		"Logging in at the consent app" \
-		"Got $code instead of 302" \
-		""
-	exit 1
-fi
+function info()
+{
+    log "info: $@"
+}
 
+function die()
+{
+    code="$1"; shift
+    log "die: $@"
+    exit $code
+}
 
-data="$(docker-compose run --rm curl \
-	-H "Cookie: oauth2_authentication_csrf=$oauth2_authentication_csrf" \
-	--cacert /etc/ssl/certs/ca-certificates.crt -i "$location")"
+function _curl()
+{
+  local ret_data origin cmd
+  [ -n "$old_location" ] || old_location="$location"
 
-oauth2_authentication_session="$(echo -e "$data" | grep -oP '(?<=: oauth2_authentication_session=)[^;]+' | tr -d '\r')"
-oauth2_consent_csrf="$(echo -e "$data" | grep -oP '(?<=: oauth2_consent_csrf=)[^;]+' | tr -d '\r')"
+  data=
+  cmd=(
+       "${default[@]}" 
+       "${cookies[@]}"
+       "$@"
+       "$location")
+  printf '\n\n'
+#  info "Running: docker-compose run --rm curl ${cmd[*]}"
+  old_location="$location"
+  data="$(docker-compose run --rm curl "${cmd[@]}")"
+#  info "Got $data"
+  printf '\n\n'
+}
+
+####################
+iinfo 1 "Asked Hydra for a Oauth2 Authentication CSRF Cookie"
+
+location="$1"
+_curl
+cookies+=( -H "Cookie: oauth2_authentication_csrf=$(echo -e "$data" | grep -oP '(?<=: oauth2_authentication_csrf=)[^;]+' | tr -d '\r')")
+
 location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
 challenge="$(echo $location | cut -d '=' -f 2)"
+
+
 code="$(echo -e "$data" | grep -oP '(?<=HTTP/2 )[0-9]+' | tr -d '\r')"
+[ "$code" == "302" ] || idie 1 "Got $code instead of 302"
+iinfo 1 "Was redirected to consent app"
 
-if [ "$code" == "302" ]; then
-	printf '[4] %s\n' \
-		"Telling Hydra we managed to login" \
-		"Getting Oauth2 Authentication Session Cookie" \
-		"Getting Oauth2 Consent CSRF Cookie" \
-		"Was redirected to consent app" \
-		""
-else
-	printf '[4] %s\n' \
-		"Telling Hydra we managed to login" \
-		"Got $code instead of 302" \
-		""
-	exit 1
-fi
+####################
+iinfo 2 "Getting the login form at the consent app" \
 
-data="$(docker-compose run --rm curl \
-	-H "Cookie: oauth2_authentication_session=$oauth2_authentication_session" \
-	-H "Cookie: oauth2_consent_csrf:$oauth2_consent_csrf" \
-	--cacert /etc/ssl/certs/ca-certificates.crt -i "$location")"
+_curl
+cookies+=( -H "Cookie: _csrf=$(echo -e "$data" | grep -oP '(?<=: _csrf=)[^;]+' | tr -d '\r')")
 
+location="$(echo $location | cut -d '?' -f 1)"
+code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
+f_csrf="$(echo -e "$data" | grep -oP '(?<=_csrf" value=")[^"]+')"
+raw="_csrf=$f_csrf&challenge=$challenge&email=$user&password=$pass&submit=Log+in"
 
-h_csrf="$(echo -e "$data" | grep -oP '(?<=: _csrf=).+$' | tr -d '\r')"
+[ "$code" == "200" ] || idie 2 "Got $code instead of 200"
+iinfo 2 "It worked, logging in"
+
+###############
+iinfo 3 "Logging in at the consent app"
+_curl \
+           -H 'Content-Type: application/x-www-form-urlencoded' \
+        --data-raw "$raw" \
+    -H "Origin: $(echo "$location" | cut -d '/' -f 1,2,3)" \
+    -H "Referer: $old_location" \
+    #
+
+referer="$(echo "$location" | cut -d '/' -f 1,2,3)/"
+location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
+
+code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
+[ "$code" == "302" ] || idie 3 "Got $code instead of 302"
+iinfo 3 "It worked, redirected towards Hydra"
+
+##############
+iinfo 4 "Telling Hydra we managed to login"
+
+_curl    -H "Referer: $referer"
+
+location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
+
+#iinfo 4 "Getting Oauth2 Authentication Session Cookie"
+#cookies+=(-H "Cookie: oauth2_authentication_session=$(echo -e "$data" | grep -oP '(?<=: oauth2_authentication_session=)[^;]+' | tr -d '\r')")
+
+iinfo 4 "Getting Oauth2 Consent CSRF Cookie"
+cookies+=(-H "Cookie: oauth2_consent_csrf=$(echo -e "$data" | grep -oP '(?<=: oauth2_consent_csrf=)[^;]+' | tr -d '\r')")
+
+challenge="$(echo $location | cut -d '=' -f 2)"
+
+code="$(echo -e "$data" | grep -oP '(?<=HTTP/2 )[0-9]+' | tr -d '\r')"
+[ "$code" == "302" ] || idie 4 "Got $code instead of 302"
+iinfo 4 "Was redirected to consent app"
+
+###############
+iinfo 5 "Fetching the form for consent at the consent app"
+
+_curl    -H "Referer: $referer"
+
+referer="$location"
 f_csrf="$(echo -e "$data" | grep -oP '(?<=_csrf" value=")[^"]+')"
 challenge="$(echo -e "$data" | grep -oP '(?<=challenge" value=")[^"]+')"
-raw="_csrf=$f_csrf&challenge=$challenge&openid=on&email=on&submit=Allow+access"
+raw="_csrf=$f_csrf&challenge=$challenge&grant_scope=openid&grant_scope=email&grant_scope=web-origins&grant_scope=role_list&grant_scope=profile&grant_scope=rules&grant_scope=address&grant_scope=phone&grant_scope=offline_access&grant_scope=microprofile-jwn&submit=Allow+access"
 
 code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
-if [ "$code" == "200" ]; then
-	printf '[5] %s\n' \
-		"Fetching the form for consent at the consent app" \
-		"Successfully" \
-		""
-else
-	printf '[5] %s\n' \
-		"Fetching the form for consent at the consent app" \
-		"Got $code instead of 200" \
-		""
-	exit 1
-fi
+[ "$code" == "200" ] || idie 5 "God $code instead of 200"
+iinfo 5 "Successfully"
 
+################
+iinfo 6 "Allowing openid,email at the consent app"
 
-data="$(docker-compose run --rm curl \
-    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
+_curl \
+    -H "Referer: $referer" \
+    -H "Origin: $(echo "$location" | cut -d '/' -f 1,2,3)" \
     -H 'Content-Type: application/x-www-form-urlencoded' \
-    -H 'Origin: https://hydra:4444' \
-    -H "Referer: $location" \
-    -H "Cookie: _csrf=$h_csrf" \
-    --data-raw "$raw" -i \
-    "$location" || true)"
+    --data-raw "$raw" \
 
+referer="$(echo "$location" | cut -d '/' -f 1,2,3)/"
 location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
 
 code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
-if [ "$code" == "302" ]; then
-	printf '[6] %s\n' \
-		"Allowing openid,email at the consent app" \
-		"Was redirected to hydra" \
-		""
-else
-	printf '[6] %s\n' \
-		"Allowing openid,email at the consent app" \
-		"Got $code instead of 302" \
-		""
-	exit 1
-fi
+[ "$code" == "302" ] || idie 6 "Got $code instead of 302"
+iinfo 6 "Was redirected to hydra"
+iinfo 6 "Allowing openid,email at the consent app"
 
+###############
+iinfo 7 "Telling Hydra we managed to consent"
 
-data="$(docker-compose run --rm curl \
-	-H "Cookie: oauth2_consent_csrf=$oauth2_consent_csrf" \
-	-H "Cookie: oauth2_authentication_csrf=$oauth2_authentication_csrf" \
-	--cacert /etc/ssl/certs/ca-certificates.crt -i "$location" || true)"
-
+_curl -H "Referer: $referer"
 location="$(echo -e "$data" | grep -oP '(?<=: )http.+$' | tr -d '\r')"
 
 code="$(echo -e "$data" | grep -oP '(?<=HTTP/2 )[0-9]+' | tr -d '\r')"
-if [ "$code" == "302" ]; then
-	printf '[7] %s\n' \
-		"Telling Hydra we managed to consent" \
-		"Using Oauth2 Authentication CSRF Cookie" \
-		"Using Oauth2 Consent CSRF Cookie" \
-		"Was redirected to callback app" \
-		""
-else
-	printf '[7] %s\n' \
-		"Telling Hydra we managed to consent" \
-		"Got $code instead of 302" \
-		""
-	exit 1
-fi
+[ "$code" == "302" ] || idie 7 "Got $code instead of 302"
+iinfo 7 "Was redirected to callback app"
 
+##############
+iinfo 8 "Giving response to the callback url"
 
-data="$(curl \
-	--cacert /etc/ssl/certs/ca-certificates.crt -i "$location" || true)"
+_curl -H "Referer: $referer"
 
 code="$(echo -e "$data" | grep -oP '(?<=HTTP/1.1 )[0-9]+' | tr -d '\r')"
-if [ "$code" == "302" ]; then
-	printf '[8] %s\n' \
-		"Giving response to the callback url" \
-	        "Successfully!" \
-		""
-else
-	printf '[8] %s\n' \
-		"Giving response to the callback url" \
-	        "Something failed with code $code" \
-		""
-	echo -e "$data"
-	exit 1
-fi
+[ "$code" == "302" ] || idie 8 "Got $code instead of 302"
+iinfo 8 "Successfully!"
