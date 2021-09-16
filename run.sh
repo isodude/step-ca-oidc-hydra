8e2839deb7777e0708dd96c0bedc6e234c7d0450
Josef Johansson <josef@oderland.se>
2021-09-16 13:57:51 +0200

    run.sh: Add caddy as SSL frontend
    Signed-off-by: Josef Johansson <josef@oderland.se>

--- run.sh
+++ run.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 set -euo pipefail
 
-docker-compose down -v || true
+printf 'Purge existing dockers..'
+out="$(docker-compose down -v 2>&1)"
+if [ $? -ne 0 ]; then
+  printf 'failed! %s\n' "$out"
+fi
+printf 'OK!\n'
 
 rm -rf step-ca conf hydra-sqlite || true
 
 mkdir -p conf
+
+cat > conf/Caddyfile <<EOF
+https://caddy:10000 {
+  tls /cert.crt /cert.key {
+    ca_root /etc/ssl/certs/ca-certificates.crt
+  }
+  reverse_proxy http://step-ca-host:10000
+}
+https://caddy:443 {
+  tls /cert.crt /cert.key {
+    ca_root /etc/ssl/certs/ca-certificates.crt
+  }
+  reverse_proxy https://step-ca
+}
+https://caddy:4444 {
+  tls /cert.crt /cert.key {
+    ca_root /etc/ssl/certs/ca-certificates.crt
+  }
+  reverse_proxy https://hydra:4444
+}
+https://caddy:3000 {
+  tls /cert.crt /cert.key {
+    ca_root /etc/ssl/certs/ca-certificates.crt
+  }
+  reverse_proxy http://consent:3000
+}
+EOF
 
 cat > conf/hydra.yaml <<EOF
 serve:
@@ -15,23 +47,23 @@ serve:
     tls:
       enabled: true
       cert:
-        path: /etc/ory/cert.crt
+        path: /cert.crt
       key:
-        path: /etc/ory/cert.key
+        path: /cert.key
   admin:
     tls:
       enabled: true
       cert:
-        path: /etc/ory/cert.crt
+        path: /cert.crt
       key:
-        path: /etc/ory/cert.key
+        path: /cert.key
 
 urls:
   self:
-    issuer: https://127.0.0.1:4444
-  consent: http://127.0.0.1:3000/consent
-  login: http://127.0.0.1:3000/login
-  logout: http://127.0.0.1:3000/logout
+    issuer: https://caddy:4444
+  consent: https://caddy:3000/consent
+  login: https://caddy:3000/login
+  logout: https://caddy:3000/logout
 
 secrets:
   system: [ $(cat /proc/sys/kernel/random/uuid) ]
@@ -53,44 +85,93 @@ cat /proc/sys/kernel/random/uuid > step-ca/secrets/provisioner-password
 
 touch conf/ca-certificates.crt
 
-docker-compose run --rm --entrypoint step step-ca ca init \
+out="$(docker-compose run --rm --entrypoint step step-ca ca init \
       --name step-ca \
       --dns step-ca \
-      --dns 127.0.0.1 \
-      --dns localhost \
       --address :443 \
       --deployment-type standalone \
       --provisioner admin \
       --provisioner-password-file /home/step/secrets/provisioner-password \
-      --password-file /home/step/secrets/password \
+      --password-file /home/step/secrets/password 2>&1 \
       #
+)"
+if [ $? -ne 0 ]; then
+  printf 'Step-ca init failed: %s\n' "$out"
+fi
 
 cat step-ca/certs/root_ca.crt > conf/ca-certificates.crt
 
-docker-compose up -d step-ca
+printf 'Starting step-ca..'
+out="$(docker-compose up -d step-ca 2>&1)"
+while [ "$(docker-compose exec step-ca step ca health 2>/dev/null | tr -d '\r\n')" != 'ok' ]; do
+  printf '.'
+  sleep 1
+done
+printf 'OK!\n'
 
-touch conf/cert.{crt,key}
 
-docker-compose run --rm hydra 'migrate' '-c' '/etc/ory/hydra.yaml' 'sql' '-e' '--yes'
-docker-compose up -d hydra
-
-docker-compose exec step-ca step ca certificate \
+out="$(docker-compose exec step-ca step ca certificate \
     --san hydra \
     --san consent \
     --san step-ca \
-    --san 127.0.0.1 \
     --san localhost \
-    --provisioner-password-file /home/step/secrets/provisioner-password step-ca '/tmp/cert.crt' '/tmp/cert.key' \
+    --san caddy \
+    --provisioner-password-file /home/step/secrets/provisioner-password step-ca '/conf/cert.crt' '/conf/cert.key' 2>&1 \
     #
+)"
+if [ $? -ne 0 ]; then
+  printf 'Step-ca certificate failed: %s\n' "$out"
+fi
 
-docker-compose exec step-ca cat /tmp/cert.crt > conf/cert.crt
-docker-compose exec step-ca cat /tmp/cert.key > conf/cert.key
+chmod a+r ./conf/cert.crt ./conf/cert.key
 
-docker-compose up -d hydra
-docker-compose up -d consent
+printf 'Starting caddy..'
+out="$(docker-compose up -d caddy 2>&1)"
+i=0
+while [ "$(docker-compose run --rm curl --cacert /etc/ssl/certs/ca-certificates.crt --write-out '%{http_code}' --output /dev/null  --silent https://caddy 2>/dev/null)" != '404' ]; do
+  if [ $i -eq 10 ]; then
+    printf 'NOT OK: %s\n %s\n' "$out" "$(docker-compose run --rm curl --cacert /etc/ssl/certs/ca-certificates.crt -i https://caddy 2>&1)"
+    exit 1
+  fi
+  printf '.'
+  i=$((i+1))
+done
+printf 'OK!\n'
+
+out="$(docker-compose run --rm hydra 'migrate' '-c' '/etc/ory/hydra.yaml' 'sql' '-e' '--yes')"
+if [ $? -ne 0 ]; then
+  printf 'Hydra init failed: %s\n' "$out"
+fi
+
+printf 'Starting hydra..'
+out="$(docker-compose up -d hydra 2>&1)"
+i=0
+while [ "$(docker-compose run --rm curl --cacert /etc/ssl/certs/ca-certificates.crt https://hydra:4444/health/ready 2>/dev/null | jq -c 2>/dev/null)" != '{"status":"ok"}' ]; do
+  if [ $i -eq 10 ]; then
+    printf 'NOT OK: %s\n %s\n' "$out" "$(docker-compose run --rm curl --cacert /etc/ssl/certs/ca-certificates.crt -i https://caddy 2>&1)"
+    exit 1
+  fi
+  printf '.'
+  i=$((i+1))
+done
+printf 'OK!\n'
+
+printf 'Starting consent..'
+out="$(docker-compose up -d consent 2>&1)"
+i=0
+while [ "$(docker-compose run --rm curl --cacert /etc/ssl/certs/ca-certificates.crt --write-out '%{http_code}' --output /dev/null  --silent http://consent:3000 2>/dev/null)" != '200' ]; do
+  if [ $i -eq 10 ]; then
+    printf 'NOT OK: %s\n %s\n' "$out" "$(docker-compose run --rm curl --cacert /etc/ssl/certs/ca-certificates.crt -i https://caddy 2>&1)"
+    exit 1
+  fi
+  printf '.'
+  i=$((i+1))
+done
+printf 'OK!\n'
 sleep 2
 
-docker-compose exec hydra hydra clients \
+printf 'Hydra: Adding step-ca client\n'
+out="$(docker-compose exec hydra hydra clients \
     create \
     --grant-types client_credentials \
     --grant-types authorization_code \
@@ -100,68 +181,46 @@ docker-compose exec hydra hydra clients \
     --token-endpoint-auth-method client_secret_post \
     --scope="openid" \
     --scope="email" \
-    --callbacks http://127.0.0.1:10000 \
-    --id step-ca \
-    #
+    --callbacks http://127.0.0.1 \
+    --id step-ca 2>&1 )"
+if [ $? -ne 0 ]; then
+  printf 'Hydra client add failed: %s\n' "$out"
+fi
 
-docker-compose exec step-ca step ca provisioner add hydra \
+out="$(docker-compose exec step-ca step ca provisioner add hydra \
     --type oidc \
     --ca-config /home/step/config/ca.json \
     --client-id step-ca \
     --client-secret step-ca \
-    --configuration-endpoint https://hydra:4444 \
-    #
+    --configuration-endpoint https://caddy:4444 2>&1 )"
+if [ $? -ne 0 ]; then
+  printf 'Hydra client add failed: %s\n' "$out"
+fi
+ 
+printf 'Step-ca: restarting\n'
+out="$(docker-compose restart step-ca 2>&1)"
+i=0
+while [ "$(docker-compose exec step-ca step ca health 2>/dev/null | tr -d '\r\n')" != 'ok' ]; do
+  if [ $i -eq 10 ]; then
+    printf 'step-ca restart failed: %s\n %s\n' "$out" "$(docker-compose exec step-ca step ca health 2>&1)"
+    exit 1
+  fi
+  printf '.'
+  i=$((i+1))
+done
+printf 'OK!\n'
 
-docker-compose restart step-ca
-
-coproc (docker-compose exec -T step-ca step ca certificate \
+printf 'Starting to auth\n'
+coproc ( docker-compose run --rm -T step-ca-host step ca certificate \
     --root /etc/ssl/certs/ca-certificates.crt \
     --provisioner hydra \
-    --ca-url https://localhost foo@bar.com /tmp/p.crt /tmp/p.key 2>&1)
-while read -r o <&"${COPROC[0]}" 2>/dev/null; do
+    --ca-url https://caddy foo@bar.com /conf/client.crt /conf/client.key 2>&1)
+
+while read -r o <&"${COPROC[0]}"; do
     if [ "${o:0:4}" == "http" ]; then
-        ./auth.sh "$o"
+      printf 'Authing to: %s\n' "$o"
+      ./auth.sh "$o"
     fi
 done 2>/dev/null
 
-docker-compose exec step-ca cat /tmp/p.crt /tmp/p.key
-
-printf 'Subject of private key: %s\n' "$(openssl x509 -in p.crt -subject -noout)"
-
-
-# step-ca will listen to 127.0.0.1 here, which obviously will not work
-#docker-compose exec step-ca step oauth \
-#    --oidc \
-#    --client-id step-ca \
-#    --client-secret step-ca \
-#    --provider https://hydra:4444 \
-#    --listen=:10000 \
-#    --redirect-url=http://127.0.0.1:10000 \
-#    #
-
-
-# Token generated!
-#    --oidc \
-#docker run \
-#    -v $PWD/conf/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt \
-#    -it \
-#    --rm \
-#    --net host smallstep/step-ca step oauth \
-#    --client-id user \
-#    --client-secret password \
-#    --provider https://127.0.0.1:4444 \
-#    --listen=127.0.0.1:10000 \
-#    --redirect-url=http://127.0.0.1:10000 \
-#    --oidc \
-#    #
-
-# Fails with
-# error parsing token: square/go-jose: missing payload in JWS message
-# Token looks like
-# {
-#  "access_token": "pa3oS1spO9gt5bya6420aLZDfa68FIW7u_NCiYx6AY8.772hTkHs7AwTuD_deVrLl9IPU3KpYcLbiqIly_PSxyU",
-#  "id_token": "",
-#  "refresh_token": "",
-#  "expires_in": 3600,
-#  "token_type": "bearer"
-#}
+printf 'Subject of private key: %s\n' "$(openssl x509 -in ./conf/client.crt -subject -noout)"
